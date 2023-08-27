ad264f1c7eaf08be0103068b0958c2c7daf0a864
shivkripaenterprises <143155836+shivkripaenterprises@users.noreply.github.com>
2023-08-27 10:58:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const header = document.querySelector("header");
+const menuToggle = document.querySelectorAll("#menu-toggle");
+
+menuToggle.forEach((toggle) => {
+  toggle.addEventListener("click", () => header.classList.toggle("showMenu"));
+});
+
+var TrandingSlider = new Swiper(".tranding-slider", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2.5,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
